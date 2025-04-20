cbaf081c1c1a59684261dad3db4c2052e6bcb6b5
KacperK2003 <kacperscizor@gmail.com>
2025-04-21 01:30:42 +0200

    Prepared basic html files.

--- App.py
+++ App.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from os import getenv
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     load_dotenv()
