5d94513fd92aca85cae53dbe5a32ade77222c5d7
KacperK2003 <kacperscizor@gmail.com>
2025-04-21 16:43:32 +0200

    Prepared two tests.

--- App.py
+++ App.py
@@ -8,6 +8,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
+
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
+
+@app.route("/test3")
+def test3():
+    return render_template('test3.html')
+
 if __name__ == '__main__':
     load_dotenv()
     app.run(debug=getenv('DEBUG'))
