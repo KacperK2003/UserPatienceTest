ee25fdf457d724f2bef668694312f2381009af83
KacperK2003 <kacperscizor@gmail.com>
2025-04-23 14:26:03 +0200

    Added basic check for existing cookies. The app can redirect to correct endpoint depending on the cookies.

--- App.py
+++ App.py
@@ -11,21 +11,38 @@ def index():
 
 @app.route('/test1')
 def test1():
+    cookie = request.cookies.get('test1')
+    if cookie:
+        return redirect(url_for('test2'))
+    
     return render_template('test1.html')
 
 @app.route('/test2')
 def test2():
+    cookie = request.cookies.get('test2')
+    if cookie:
+        return redirect(url_for('test3'))
+    
     return render_template('test2.html')
 
 @app.route('/test3')
 def test3():
+    cookie = request.cookies.get('test3')
+    if cookie:
+        return redirect(url_for('summary'))
     return render_template('test3.html')
 
 @app.route('/summary')
 def summary():
     time1 = request.cookies.get('test1')
+    if not time1:
+        return redirect(url_for('test1'))
     time2 = request.cookies.get('test2')
+    if not time2:
+        return redirect(url_for('test2'))
     time3 = request.cookies.get('test3')
+    if not time3:
+        return redirect(url_for('test3'))
     return f'{time1}, {time2}, {time3}'
 
 if __name__ == '__main__':
